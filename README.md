119ac26dc97f4540408cc03eb4e7d4249f32daf7
Atif Galamov <kalemli@gmail.com>
2024-09-14 19:00:30 +0400

    bugfix: postgres DVD Rental sample db

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # sample-databases
+
+## PostgreSQL
+
+### sample db link - DVDRental
+
+https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
--- postgres/start.sh
+++ postgres/start.sh
@@ -1,4 +1,4 @@
 docker compose down
 docker compose rm -f
 docker compose up -d
-docker exec -i postgres_db /bin/sh < ./restore.sh
+docker exec -i postgres_db /bin/sh < restore.sh
